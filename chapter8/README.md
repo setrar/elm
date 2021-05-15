390e0566e9a9b21cbe3c2ae9467fa7fc20f4f03d
u0000000021 <u0000000021@uontario.ca>
2021-05-14 22:05:39 -0400

    .

--- chapter8/README.md
+++ chapter8/README.md
@@ -1,1 +1,76 @@
+# ELM in Action
+
+https://www.manning.com/books/elm-in-action
+
+
+## :a: Create the project
+
+:round_pushpin: Initialize an `elm` project
+
+```
+$ elm init
+```
+
+:round_pushpin: Add required libraries
+
+- [ ] Add the [`Random`](https://package.elm-lang.org/packages/elm/random/latest/) library
+
+```
+$ echo Y | elm install elm/random
+```
+
+- [ ] Add the [`Http`](https://package.elm-lang.org/packages/elm/http/latest/) library
+
+```
+$ echo Y | elm install elm/http
+```
+
+- [ ] Add the [`Json`](https://package.elm-lang.org/packages/elm/json/latest/) library
+
+```
+$ echo Y | elm install elm/json 
+```
+
+- [ ] Add the [`NoRedInk/elm-json-decode-pipeline`](https://package.elm-lang.org/packages/NoRedInk/elm-json-decode-pipeline/latest) library
+
+
+```
+$ echo Y | elm install NoRedInk/elm-json-decode-pipeline
+```
+
+:round_pushpin:  Create and Compile the application
+
+- [ ] Compile the application
+
+```
+$ elm make src/PhotoFolder.elm src/PhotoGroove.elm --output app.js    
+```
+
+- [ ] Display the application
+
+```html
+<html>
+    <head>
+        <link rel="stylesheet" href="http://elm-in-action.com/styles.css">
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="app.js"></script>
+        <script>
+            Elm.PhotoFolder.init( { node: document.getElementById("app")} );
+        </script>
+    </body>
+</html>
+```
+
+
+## :ab: Run the project
+
+```
+$ elm reactor
+```
+
+test on:
+
+http://localhost:8000
 
---
+++ chapter8/src/PhotoGroove.elm
@@ -0,0 +1,278 @@
+port module PhotoGroove exposing (main, photoDecoder)
+
+import Browser
+import Html exposing (Attribute, Html, button, div, h1, h3, img, input, label, node, text, canvas)
+import Html.Attributes as Attr exposing (class, classList, id, name, src, title, type_)
+import Html.Events exposing (on, onClick)
+import Random
+import Http
+import Json.Decode exposing (Decoder, at, int, list, string, succeed)
+import Json.Decode.Pipeline exposing (optional, required)
+import Json.Encode as Encode
+
+urlPrefix : String
+urlPrefix =
+   "http://elm-in-action.com/"
+
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotRandomPhoto Photo
+    | GotActivity String
+    | GotPhotos (Result Http.Error (List Photo))
+    | SlidHue Int
+    | SlidRipple Int
+    | SlidNoise Int
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ] <|
+        case model.status of
+            Loaded photos selectedUrl ->
+                viewLoaded photos selectedUrl model
+
+            Loading ->
+                []
+
+            Errored errorMessage ->
+                [ text ("Error: " ++ errorMessage) ]
+
+viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
+viewFilter toMsg name magnitude =
+    div [ class "filter-slider" ]
+        [ label [] [ text name ]
+        , rangeSlider
+            [ Attr.max "11"
+            , Attr.property "val" (Encode.int magnitude)
+            , onSlide toMsg
+            ]
+            []
+        , label [] [ text (String.fromInt magnitude) ]
+        ]
+
+viewLoaded : List Photo -> String -> Model -> List (Html Msg)
+viewLoaded photos selectedUrl model =
+        [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick ClickedSurpriseMe ]
+            [ text "Surprise Me!" ]
+        , div [ class "activity" ] [ text model.activity ]
+        , div [ class "filters" ]
+            [ viewFilter SlidHue "Hue" model.hue
+            , viewFilter SlidRipple "Ripple" model.ripple
+            , viewFilter SlidNoise "Noise" model.noise
+            ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
+            (List.map (viewThumbnail selectedUrl) photos)
+        , canvas [ id "main-canvas", class "large"] []
+        ]
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]" )
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick <| ClickedPhoto thumb.url
+        ]
+        []
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+        , text (sizeToString size)
+        ]
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+port setFilters : FilterOptions -> Cmd msg
+
+port activityChanges : (String -> msg) -> Sub msg
+
+type alias FilterOptions =
+    { url : String
+    , filters : List { name : String, amount : Float }
+    }
+
+type alias Photo =
+    { url : String
+    , size: Int
+    , title : String
+    }
+
+photoDecoder : Decoder Photo
+photoDecoder =
+    succeed Photo
+        |> required "url" string
+        |> required "size" int
+        |> optional "title" string "(untitled)"
+
+type Status
+    = Loading
+    | Loaded (List Photo) String
+    | Errored String
+
+type alias Model =
+    { status : Status
+    , activity : String
+    , chosenSize : ThumbnailSize
+    , hue : Int
+    , ripple : Int
+    , noise : Int
+    }
+
+initialModel : Model
+initialModel =
+    { status = Loading
+    , activity = ""
+    , chosenSize = Medium
+    , hue = 5
+    , ripple = 5
+    , noise = 5
+    }
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotActivity activity ->
+            ( { model | activity = activity }, Cmd.none)
+
+        GotRandomPhoto photo ->
+            applyFilters { model | status = selectUrl photo.url model.status }
+
+        ClickedPhoto url ->
+            applyFilters { model | status = selectUrl url model.status }
+
+        ClickedSize size ->
+            ( { model | chosenSize = size }, Cmd.none )
+
+        ClickedSurpriseMe ->
+            case model.status of
+                Loaded (x :: xs) _ ->
+                    Random.uniform x xs
+                    |> Random.generate GotRandomPhoto
+                    |> Tuple.pair model
+
+                Loaded [] _ ->
+                    ( model, Cmd.none)
+
+                Loading ->
+                    ( model, Cmd.none )
+
+                Errored _ ->
+                    ( model, Cmd.none )
+
+        GotPhotos (Ok photos) ->
+            case photos of
+                x :: _ ->
+                    applyFilters
+                    { model
+                        | status =
+                            case List.head photos of
+                                Just photo ->
+                                    Loaded photos x.url
+
+                                Nothing ->
+                                    Loaded [] ""
+                    }
+                [] ->
+                    ( { model | status = Errored "0 photos found" }, Cmd.none )
+
+        GotPhotos (Err httpError) ->
+            ( { model | status = Errored "Server error!" }, Cmd.none )
+
+        SlidHue hue ->
+            applyFilters { model | hue = hue }
+
+        SlidRipple ripple ->
+            applyFilters { model | ripple = ripple }
+
+        SlidNoise noise ->
+            applyFilters { model | noise = noise }
+
+applyFilters : Model -> ( Model, Cmd Msg)
+applyFilters model =
+    case model.status of
+        Loaded photos selectedUrl ->
+            let
+                filters =
+                    [ { name = "Hue", amount = toFloat model.hue / 11 }
+                    , { name = "Ripple", amount = toFloat model.ripple / 11 }
+                    , { name = "Noise", amount = toFloat model.noise / 11 }
+                    ]
+                url =
+                    urlPrefix ++ "large/" ++ selectedUrl
+            in
+            ( model, setFilters { url = url, filters = filters } )
+
+        Loading ->
+            ( model, Cmd.none )
+
+        Errored errorMessage ->
+            ( model, Cmd.none )
+
+selectUrl : String -> Status -> Status
+selectUrl url status =
+    case status of
+        Loaded photos _ ->
+            Loaded photos url
+
+        Loading ->
+            status
+
+        Errored errorMessage ->
+            status
+
+initialCmd : Cmd Msg
+initialCmd =
+    Http.get
+        { url = urlPrefix ++ "/photos/list.json"
+        , expect = Http.expectJson GotPhotos (list photoDecoder)
+        }
+
+main : Program Float Model Msg
+main = 
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> activityChanges GotActivity
+        }
+
+init : Float -> ( Model, Cmd Msg )
+init flags =
+    let
+        activity =
+            "Initializing Pasta v" ++ String.fromFloat flags
+    in
+    ( { initialModel | activity = activity }, initialCmd )
+
+rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
+rangeSlider attributes children =
+    node "range-slider" attributes children
+
+onSlide : (Int -> msg) -> Attribute msg
+onSlide toMsg =
+    at [ "detail", "userSlidTo" ] int
+    |> Json.Decode.map toMsg
+    |> on "slide"
